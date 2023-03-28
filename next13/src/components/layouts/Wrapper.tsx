1e5168e4f195b829a8fe276d3f068e78f2767003
kdilot <kdilotdev@gmail.com>
2023-03-29 08:01:24 +0900

    remove

--- next13/src/components/layouts/Wrapper.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-import React from 'react'
-
-const Wrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  return <div className="w-full h-[100vh] bg-red-200 px-8 sm:px-[20%] lg:px-[30%] overflow-auto">{children}</div>
-}
-
-export default Wrapper
